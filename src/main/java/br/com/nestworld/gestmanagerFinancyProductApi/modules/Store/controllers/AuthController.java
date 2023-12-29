05b705bdaebb766760a8f9a75c8b55aebf323a4a
eduardo i nakai frasson <109884797+eduardonakaidev@users.noreply.github.com>
2023-12-29 19:43:34 -0300

    alterei o fluxo de autenticacao

--- src/main/java/br/com/nestworld/gestmanagerFinancyProductApi/modules/Store/controllers/AuthController.java
+++ src/main/java/br/com/nestworld/gestmanagerFinancyProductApi/modules/Store/controllers/AuthController.java
@@ -1,9 +1,11 @@
 package br.com.nestworld.gestmanagerFinancyProductApi.modules.Store.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,10 +35,15 @@ public class AuthController {
 
     @PostMapping("/auth")
     public ResponseEntity<Object> AuthStore(@RequestBody @Valid AuthRequestDTO reqAuth){
+        try{
        var emailPassword = new UsernamePasswordAuthenticationToken(reqAuth.email(),reqAuth.password());
         var auth = this.authenticationManager.authenticate(emailPassword);
         var token = tokenService.generateToken((StoreEntity)auth.getPrincipal());
         return ResponseEntity.ok(new AuthReponseDTO(token));
+         } catch (AuthenticationException e) {
+        // Trate a exceção de autenticação aqui
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
     }
 
     @PostMapping("/register")
